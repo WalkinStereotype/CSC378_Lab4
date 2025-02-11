d49eb33156a3c9a3cca3941e51e701d60441b9f3
Josh Hall <113940171+Agwe422@users.noreply.github.com>
2025-02-10 16:17:08 -0800

    player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D myRigidbody;
+    public SpriteRenderer sr;
 
+    public float moveSpeed;
+    public float jumpSpeed;
+    public bool isGrounded = false;
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal"); //horizontal input (1 or -1)
+        float currentVerticalVelocity = myRigidbody.linearVelocity.y; //current vertical velocity
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded){
+            currentVerticalVelocity = jumpSpeed;
+            isGrounded = false;
+            // sr.color = Color.red;
+        }
         
+        Vector2 newVelocity = new Vector2(horizontalInput * moveSpeed, currentVerticalVelocity);
+        myRigidbody.linearVelocity = newVelocity;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+            isGrounded = true;
+            // sr.color = Color.green;
+        }
     }
 }
