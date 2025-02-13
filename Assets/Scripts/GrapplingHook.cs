919459d8f4d0da827a7ee103a4ef6a5d5868ca41
Dre Suguitan <ddre2026@gmail.com>
2025-02-12 23:26:47 -0800

    Rendered simple grappling rope, removed supporting and other test platforms

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -4,6 +4,7 @@ public class GrapplingHook : MonoBehaviour
 {
     [SerializeField] private float grappleLength;
     [SerializeField] private LayerMask grappleLayer;
+    [SerializeField] private LineRenderer rope;
 
     private Vector3 grapplePoint;
     private DistanceJoint2D joint;
@@ -13,6 +14,7 @@ public class GrapplingHook : MonoBehaviour
     {
         joint = gameObject.GetComponent<DistanceJoint2D>();
         joint.enabled = false;
+        rope.enabled = false;
     }
 
     // Update is called once per frame
@@ -34,11 +36,19 @@ public class GrapplingHook : MonoBehaviour
                 joint.connectedAnchor = grapplePoint; //set anchor of grappling hook (for swinging)
                 joint.enabled = true;
                 joint.distance = grappleLength; //distance of the grapple rope
+                rope.SetPosition(0, grapplePoint);
+                rope.SetPosition(1, transform.position);
+                rope.enabled = true;
             }
         }
 
         if(Input.GetMouseButtonUp(0)){
             joint.enabled = false;
+            rope.enabled = false;
+        }
+
+        if(rope.enabled){
+            rope.SetPosition(1, transform.position);
         }
     }
 }
