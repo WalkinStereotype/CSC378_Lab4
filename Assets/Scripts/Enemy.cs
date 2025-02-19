245b86ec8a115a63f336eaa305fa11afb037ed1d
Dre Suguitan <ddre2026@gmail.com>
2025-02-18 20:37:38 -0800

    Enemy faces player when player enters fov

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,13 +2,41 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-public int maxHealth = 100;
-int currentHealth;
+    public int maxHealth = 100;
+    int currentHealth;
+
+    public Transform player;
+    private SpriteRenderer spriteRenderer;
+
+    private EnemyFOV enemyFOV;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        enemyFOV = GetComponent<EnemyFOV>();
         currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        if (enemyFOV.canSeePlayer)
+        {
+            FlipTowardsPlayer();
+        }
+    }
+
+    void FlipTowardsPlayer()
+    {
+        if (player.position.x < transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0); // Face right
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0); // Face left
+        }
     }
 
     public void TakeDamage(int damage)
