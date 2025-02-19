44948152f2857d623e8ff11025e37f7a30f9a28c
Dre Suguitan <ddre2026@gmail.com>
2025-02-19 03:08:07 -0800

    Enemy Shooting upon seeing player, no collision yet

---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    private float timer;
+    public float bulletCooldown = 2f;
+
+    private EnemyFOV enemyFOV;
+
+    
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        enemyFOV = GetComponent<EnemyFOV>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > bulletCooldown){
+            timer = 0;
+            if (enemyFOV.canSeePlayer){
+                shoot();
+            }
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+}
