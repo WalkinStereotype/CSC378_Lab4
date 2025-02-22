719bf2c018698b608a53109da22c40de66eb7b00
Dre Suguitan <ddre2026@gmail.com>
2025-02-21 16:09:12 -0800

    Player can skip intro with pressing enter

--- Assets/Scripts/Scene/CutSceneController.cs
+++ Assets/Scripts/Scene/CutSceneController.cs
@@ -5,15 +5,35 @@ using UnityEngine.SceneManagement;
 public class CutsceneController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
-    public string nextSceneName = "MainMenu"; // Change this to your actual Main Menu scene name
+    public string nextSceneName = "MainMenu"; // Change this to your actual menu scene
 
     void Start()
     {
-        videoPlayer.loopPointReached += OnVideoEnd; // Subscribe to the event
+        videoPlayer.loopPointReached += OnVideoEnd; // Automatically load next scene when the video ends
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) // Detect Enter key press
+        {
+            SkipCutscene();
+        }
     }
 
     void OnVideoEnd(VideoPlayer vp)
     {
-        SceneManager.LoadScene(nextSceneName); // Load Main Menu when video ends
+        LoadNextScene();
+    }
+
+    void SkipCutscene()
+    {
+        Debug.Log("Cutscene skipped!");
+        videoPlayer.Stop(); // Stop the video playback
+        LoadNextScene();
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
     }
 }
